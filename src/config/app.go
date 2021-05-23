5e46d2536277a274e9525e3eebc170058e2db31a
Jose Salas <josephsalas.developer@gmail.com>
2021-05-23 16:23:30 -0500

    refactor .env and mysql connection

--- src/config/app.go
+++ src/config/app.go
@@ -3,4 +3,10 @@ package config
 import "os"
 
 var Port = os.Getenv("PORT")
-var MysqlDns = os.Getenv("MYSQL_DNS")
+
+// Mysql config
+var MysqlDataBase = os.Getenv("MYSQL_DATABASE")
+var MysqlUser = os.Getenv("MYSQL_USER")
+var MysqlPassword = os.Getenv("MYSQL_PASSWORD")
+var MysqlHost = os.Getenv("MYSQL_HOST")
+var MysqlPort = os.Getenv("MYSQL_PORT")
--- src/models/db.go
+++ src/models/db.go
@@ -3,6 +3,8 @@
 package models
 
 import (
+	"fmt"
+
 	"github.com/josesalasdev/golang_api_template/src/config"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -11,7 +13,16 @@ import (
 var DB *gorm.DB
 
 func ConnectDataBase() {
-	db, err := gorm.Open(mysql.Open(config.MysqlDns), &gorm.Config{})
+	mysqlDns := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		config.MysqlUser,
+		config.MysqlPassword,
+		config.MysqlHost,
+		config.MysqlPort,
+		config.MysqlDataBase,
+	)
+	fmt.Println(mysqlDns)
+	db, err := gorm.Open(mysql.Open(mysqlDns), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
