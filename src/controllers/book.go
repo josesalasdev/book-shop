4223d3893361d59ad591b7c635c1e59e49fd0ad7
Jose Salas <josephsalas.developer@gmail.com>
2021-05-23 19:16:17 -0500

    add create book and detail book

--- src/controllers/book.go
+++ src/controllers/book.go
@@ -12,15 +12,58 @@ import (
 // GET /books
 // Get all books
 // Ping godoc
-// @Summary Find Books.
+// @Summary Find books.
 // @Description Get all books.
 // @Accept  json
 // @Produce  json
 // @Success 200 {object} []models.Book
-// @Router /books [get]
+// @Router /book/ [get]
 func FindBooks(c *gin.Context) {
 	var books []models.Book
 	models.DB.Find(&books)
 
 	c.JSON(http.StatusOK, gin.H{"data": books})
+}
+
+// DetailBooks godoc
+// @Summary Detail of a book.
+// @Description Detail of a book by ID
+// @ID get-string-by-int
+// @Accept  json
+// @Produce  json
+// @Param id path int true "Book ID"
+// @Success 200 {object} models.Book
+// @Router /book/{id}/ [get]
+func DetailBooks(c *gin.Context) {
+	var book models.Book
+
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Book not found!"})
+		return
+	}
+
+	c.JSON(http.StatusOK, book)
+}
+
+// CreateBook godoc
+// @Summary Create a book.
+// @Description Create a book.
+// @Accept  json
+// @Produce  json
+// @Param data body models.CreateBookInput true "Book Data"
+// @Success 200 {object} models.Book
+// @Router /book/ [post]
+func CreateBook(c *gin.Context) {
+	// Validate input
+	var input models.CreateBookInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Create book
+	book := models.Book{Title: input.Title, Price: input.Price}
+	models.DB.Create(&book)
+
+	c.JSON(http.StatusCreated, input)
 }
